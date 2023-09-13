750ce0d0bb57d0af0f7a61995668d077e3279498
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-09-13 16:39:05 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+  type = "map"
+
+  default = {
+    "us-east-1" = "ami-053b0d53c279acc90"
+    "ap-south-1" = "ami-05552d2dcf89c9b24"
+  }
+}
+
+variable "instance_count" {
+  default = "2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
